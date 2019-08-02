c0610dabf5e0207cf7308ad6d9a8c50af2b819fc
Aakash074 <aakashkumaar074@gmail.com>
2019-08-02 19:44:49 +0530

    mera msg

---
+++ truffle.js
@@ -0,0 +1,11 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+module.exports = {
+    azureNetwork: {
+      host: 'xyz.com',
+	  port: xxxx,
+      network_id: "xxx",
+      }
+  }
+}
